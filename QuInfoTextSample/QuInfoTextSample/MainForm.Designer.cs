599bb3998d20d1869092c8acc646e5b011d9a1b8
みらい <105960405+0mirai1@users.noreply.github.com>
2022-05-21 14:00:52 +0900

    Add files via upload

---
+++ QuInfoTextSample/QuInfoTextSample/MainForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace QuInfoTextSample
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.QuInfoTimer = new System.Windows.Forms.Timer(this.components);
+            this.EpicenterLabel = new System.Windows.Forms.Label();
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.TimeLabel = new System.Windows.Forms.Label();
+            this.MaxintLabel = new System.Windows.Forms.Label();
+            this.DepthLabel = new System.Windows.Forms.Label();
+            this.MagnitubeLabel = new System.Windows.Forms.Label();
+            this.CityInfoRichTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.CommentLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // QuInfoTimer
+            // 
+            this.QuInfoTimer.Enabled = true;
+            this.QuInfoTimer.Interval = 5000;
+            this.QuInfoTimer.Tick += new System.EventHandler(this.QuInfoTimer_Tick);
+            // 
+            // EpicenterLabel
+            // 
+            this.EpicenterLabel.AutoSize = true;
+            this.EpicenterLabel.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.EpicenterLabel.Location = new System.Drawing.Point(12, 62);
+            this.EpicenterLabel.Name = "EpicenterLabel";
+            this.EpicenterLabel.Size = new System.Drawing.Size(59, 13);
+            this.EpicenterLabel.TabIndex = 0;
+            this.EpicenterLabel.Text = "震源地名";
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.TitleLabel.Location = new System.Drawing.Point(12, 9);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(45, 13);
+            this.TitleLabel.TabIndex = 1;
+            this.TitleLabel.Text = "タイトル";
+            // 
+            // TimeLabel
+            // 
+            this.TimeLabel.AutoSize = true;
+            this.TimeLabel.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.TimeLabel.Location = new System.Drawing.Point(12, 35);
+            this.TimeLabel.Name = "TimeLabel";
+            this.TimeLabel.Size = new System.Drawing.Size(59, 13);
+            this.TimeLabel.TabIndex = 2;
+            this.TimeLabel.Text = "発生時刻";
+            // 
+            // MaxintLabel
+            // 
+            this.MaxintLabel.AutoSize = true;
+            this.MaxintLabel.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.MaxintLabel.Location = new System.Drawing.Point(12, 89);
+            this.MaxintLabel.Name = "MaxintLabel";
+            this.MaxintLabel.Size = new System.Drawing.Size(59, 13);
+            this.MaxintLabel.TabIndex = 3;
+            this.MaxintLabel.Text = "最大震度";
+            // 
+            // DepthLabel
+            // 
+            this.DepthLabel.AutoSize = true;
+            this.DepthLabel.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.DepthLabel.Location = new System.Drawing.Point(12, 115);
+            this.DepthLabel.Name = "DepthLabel";
+            this.DepthLabel.Size = new System.Drawing.Size(29, 13);
+            this.DepthLabel.TabIndex = 4;
+            this.DepthLabel.Text = "深さ";
+            // 
+            // MagnitubeLabel
+            // 
+            this.MagnitubeLabel.AutoSize = true;
+            this.MagnitubeLabel.Font = new System.Drawing.Font("MS UI Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.MagnitubeLabel.Location = new System.Drawing.Point(12, 142);
+            this.MagnitubeLabel.Name = "MagnitubeLabel";
+            this.MagnitubeLabel.Size = new System.Drawing.Size(76, 13);
+            this.MagnitubeLabel.TabIndex = 5;
+            this.MagnitubeLabel.Text = "マグニチュード";
+            // 
+            // CityInfoRichTextBox1
+            // 
+            this.CityInfoRichTextBox1.Location = new System.Drawing.Point(15, 176);
+            this.CityInfoRichTextBox1.Name = "CityInfoRichTextBox1";
+            this.CityInfoRichTextBox1.ReadOnly = true;
+            this.CityInfoRichTextBox1.Size = new System.Drawing.Size(536, 164);
+            this.CityInfoRichTextBox1.TabIndex = 6;
+            this.CityInfoRichTextBox1.Text = "";
+            // 
+            // CommentLabel
+            // 
+            this.CommentLabel.AutoSize = true;
+            this.CommentLabel.Location = new System.Drawing.Point(203, 10);
+            this.CommentLabel.Name = "CommentLabel";
+            this.CommentLabel.Size = new System.Drawing.Size(51, 12);
+            this.CommentLabel.TabIndex = 7;
+            this.CommentLabel.Text = "comment";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 352);
+            this.Controls.Add(this.CommentLabel);
+            this.Controls.Add(this.CityInfoRichTextBox1);
+            this.Controls.Add(this.MagnitubeLabel);
+            this.Controls.Add(this.DepthLabel);
+            this.Controls.Add(this.MaxintLabel);
+            this.Controls.Add(this.TimeLabel);
+            this.Controls.Add(this.TitleLabel);
+            this.Controls.Add(this.EpicenterLabel);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer QuInfoTimer;
+        private System.Windows.Forms.Label EpicenterLabel;
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.Label TimeLabel;
+        private System.Windows.Forms.Label MaxintLabel;
+        private System.Windows.Forms.Label DepthLabel;
+        private System.Windows.Forms.Label MagnitubeLabel;
+        private System.Windows.Forms.RichTextBox CityInfoRichTextBox1;
+        private System.Windows.Forms.Label CommentLabel;
+    }
+}
+
---
+++ QuInfoTextSample/QuInfoTextSample/MainForm.cs
@@ -0,0 +1,230 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuInfoTextSample
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private  void MainForm_Load(object sender, EventArgs e)
+        {
+            Check();
+        }
+        string Last_eventid = "0";
+        string Last_dateTime = "0";
+        string Webapiurl = "https://dev.narikakun.net/webapi/earthquake/post_data.json?_ga=2.102905280.1911846031.1635327567-1306914555.1619779464";//←API
+        private async void QuInfoTimer_Tick(object sender, EventArgs e)
+        {
+            //API上5000ミリ秒(5秒間隔)で取得
+            //QuInfoにアクセス
+            await Task.Run(() => Check());
+        }
+        private void Check()
+        {
+            try
+            {
+                WebClient WebC = new WebClient() { Encoding = Encoding.GetEncoding("UTF-8") };
+                string Api = WebC.DownloadString(Webapiurl);
+                try
+                {
+                    var Json = JObject.Parse(Api);
+                    if (Json["Control"]["DateTime"].Value<string>() == Last_dateTime) return;
+                    string cityint1 = "", cityint2 = "", cityint3 = "", cityint4 = "", cityint5 = "", cityint5plus = "", cityint6 = "", cityint6plus = "", cityint7 = "", cityintF = "";
+                    if (Last_eventid ==Json["Head"]["EventID"].Value<string>())
+                    {
+                        if (Json["Head"]["Title"].Value<string>() != "震源に関する情報")
+                        {
+                            CityInfoRichTextBox1.Text = "";
+                        }
+                    }
+                    else
+                    {
+                        CityInfoRichTextBox1.Text = "";
+                    }
+                    if (Json["Head"]["Title"].Value<string>() != "震源に関する情報")
+                    {
+                        foreach (JObject pref in Json["Body"]["Intensity"]["Observation"]["Pref"])
+                        {
+                      CityInfoRichTextBox1.Text += pref["Name"].Value<string>() + "：";
+                          
+                            foreach (JObject area in pref["Area"])
+                            {
+                                if (Json["Head"]["Title"].Value<string>() == "震源・震度情報")
+                                {
+                                    foreach (JObject city in area["City"])
+                                    {
+                                        foreach (JObject stationin in city["IntensityStation"])
+                                        {
+                                            switch(stationin["Int"].Value<string>())
+                                            {
+                                                case "1":
+                                                    cityint1 += stationin["Name"].Value<string>().Replace("＊", "") + " ";
+                                                        break;
+                                                case "2":
+                                                    cityint2 += stationin["Name"].Value<string>().Replace("＊", "") + " ";
+                                                    break;
+                                                case "3":
+                                                    cityint3 += stationin["Name"].Value<string>().Replace("＊", "") + " ";
+                                                    break;
+                                                case "4":
+                                                    cityint4 += stationin["Name"].Value<string>().Replace("＊", "") + " ";
+                                                    break;
+                                                case "5-":
+                                                    cityint5 += stationin["Name"].Value<string>().Replace("＊", "") + " ";
+                                                    break;
+                                                case "5+":
+                                                    cityint5plus += stationin["Name"].Value<string>().Replace("＊", "") + " ";
+                                                    break;
+                                                case "6-":
+                                                    cityint6 += stationin["Name"].Value<string>().Replace("＊", "") + " ";
+                                                    break;
+                                                case "6+":
+                                                    cityint6plus += stationin["Name"].Value<string>().Replace("＊", "") + " ";
+                                                    break;
+                                                case "7":
+                                                    cityint7 += stationin["Name"].Value<string>().Replace("＊", "") + " ";
+                                                    break;
+                                                default:
+                                                    cityintF += stationin["Name"].Value<string>().Replace("＊", "") + " ";
+                                                    break;
+                                            }
+                                        }
+                                    }
+                                }
+                                else if (Json["Head"]["Title"].Value<string>() == "震度速報")
+                                {
+                                    switch(area["MaxInt"].Value<string>())
+                                    {
+                                        case "1":
+                                            cityint1 += area["Name"].Value<string>() + " ";
+                                            break;
+                                        case "2":
+                                            cityint2 += area["Name"].Value<string>() + " ";
+                                            break;
+                                        case "3":
+                                            cityint3 += area["Name"].Value<string>() + " ";
+                                            break;
+                                        case "4":
+                                            cityint4 += area["Name"].Value<string>() + " ";
+                                            break;
+                                        case "5-":
+                                            cityint5 += area["Name"].Value<string>() + " ";
+                                            break;
+                                        case "5+":
+                                            cityint5plus += area["Name"].Value<string>() + " ";
+                                            break;
+                                        case "6-":
+                                            cityint6 += area["Name"].Value<string>() + " ";
+                                            break;
+                                        case "6+":
+                                            cityint6plus += area["Name"].Value<string>() + " ";
+                                            break;
+                                        case "7":
+                                            cityint7 += area["Name"].Value<string>() + " ";
+                                            break;
+                                        default:
+                                            cityintF += area["Name"].Value<string>() + " ";
+                                            break;
+                                    }
+                                }
+                            }
+                            if (cityintF != "") { CityInfoRichTextBox1.Text += "【震度5弱以上未入電】" + cityintF; }
+                            if (cityint7 != "") { CityInfoRichTextBox1.Text += "【震度7】" + cityint7; }
+                            if (cityint6plus != "") { CityInfoRichTextBox1.Text += "【震度6強】" + cityint6plus; }
+                            if (cityint6 != "") { CityInfoRichTextBox1.Text += "【震度6弱】" + cityint6; }
+                            if (cityint5plus != "") { CityInfoRichTextBox1.Text += "【震度5強】" + cityint5plus; }
+                            if (cityint5 != "") { CityInfoRichTextBox1.Text += "【震度5弱】" + cityint5; }
+                            if (cityint4 != "") { CityInfoRichTextBox1.Text += "【震度4】" + cityint4; }
+                            if (cityint3 != "") { CityInfoRichTextBox1.Text += "【震度3】" + cityint3; }
+                            if (cityint2 != "") { CityInfoRichTextBox1.Text += "【震度2】" + cityint2; }
+                            if (cityint1 != "") { CityInfoRichTextBox1.Text += "【震度1】" + cityint1; }
+                            cityint1 = "";
+                            cityint2 = "";
+                            cityint3 = "";
+                            cityint4 = "";
+                            cityint5 = "";
+                            cityint5plus = "";
+                            cityint6 = "";
+                            cityint6plus = "";
+                            cityint7 = "";
+                            cityintF = "";
+                            CityInfoRichTextBox1.Text += "\r\n";
+                        }
+                    }
+                    Console.WriteLine(Json["Body"]["Comments"]["Observation"]);
+                    if (Json["Head"]["Title"].Value<string>() == "遠地地震に関する情報")
+                    {
+                        string d = Json["Body"]["Earthquake"]["ArrivalTime"].Value<string>();
+                        string f = "yyyy-MM-dd HH:mm:ss";
+                        DateTime dt = DateTime.ParseExact(d, f, null);
+                        TimeLabel.Text = dt.ToString("dd日hh時mm分") + "　発生";
+                        MaxintLabel.Text = "遠地地震";
+                        CityInfoRichTextBox1.Text = dt.ToString("dd日hh時mm分ごろ") + "海外で地震がありました。震源地は" + Json["Body"]["Earthquake"]["Hypocenter"]["Name"].Value<string>() + "で、震源の深さは" + Json["Body"]["Earthquake"]["Hypocenter"]["Depth"].Value<string>() + "km、地震の規模を示すマグニチュードは" + Json["Body"]["Earthquake"]["Magnitude"].Value<string>() + "と推定されます。";
+                    }
+                    if (Json["Head"]["Title"].Value<string>() == "震源・震度情報")
+                    {
+                        string d = Json["Head"]["TargetDateTime"].Value<string>();
+                        string f = "yyyy-MM-dd HH:mm:ss";
+                        DateTime dt = DateTime.ParseExact(d, f, null);
+                       TimeLabel.Text = dt.ToString("dd日hh時mm分") + "　発生";
+                        EpicenterLabel.Text = "震源地名　"+Json["Body"]["Earthquake"]["Hypocenter"]["Name"].Value<string>();
+                        DepthLabel.Text = "深さ　" + Json["Body"]["Earthquake"]["Hypocenter"]["Depth"].Value<string>()+"km";
+                        MagnitubeLabel.Text = "マグニチュード　" + Json["Body"]["Earthquake"]["Magnitude"].Value<string>();
+                        MaxintLabel.Text = "最大震度" + Json["Body"]["Intensity"]["Observation"]["MaxInt"].Value<string>();
+                    }
+                    else if (Json["Head"]["Title"].Value<string>() == "震度速報")
+                    {
+                        MaxintLabel.Text = "最大震度 " + Json["Body"]["Intensity"]["Observation"]["MaxInt"].Value<string>().Replace("-", "弱").Replace("+", "強");
+                        string d = Json["Head"]["TargetDateTime"].Value<string>();
+                        string f = "yyyy-MM-dd HH:mm:ss";
+                        DateTime dt = DateTime.ParseExact(d, f, null); TimeLabel.Text = dt.ToString("dd日hh時mm分") + "　発生";
+                        EpicenterLabel.Text = "調査中";
+                        DepthLabel.Text = "";
+                        MagnitubeLabel.Text = "";
+                        DepthLabel.Visible = false;
+                        MagnitubeLabel.Visible = false;
+                    }
+                    if (Json["Head"]["Title"].Value<string>() == "震源に関する情報")
+                    {
+                        string d = Json["Head"]["TargetDateTime"].Value<string>();
+                        string f = "yyyy-MM-dd HH:mm:ss";
+                        DateTime dt = DateTime.ParseExact(d, f, null);
+                        TimeLabel.Text = dt.ToString("dd日hh時mm分") + "　発生";
+                        EpicenterLabel.Text = "震源地名　" + Json["Body"]["Earthquake"]["Hypocenter"]["Name"].Value<string>();
+                        DepthLabel.Text = "深さ　" + Json["Body"]["Earthquake"]["Hypocenter"]["Depth"].Value<string>()+"km";
+                        MagnitubeLabel.Text = "マグニチュード　" + Json["Body"]["Earthquake"]["Magnitude"].Value<string>();
+                        MaxintLabel.Text = "";
+           
+                        CityInfoRichTextBox1.Text = dt.ToString("dd日hh時mm分ごろ") + "地震がありました。震源地は" + Json["Body"]["Earthquake"]["Hypocenter"]["Name"].Value<string>() + "で、震源の深さは" + Json["Body"]["Earthquake"]["Hypocenter"]["Depth"].Value<string>() + "km、地震の規模を示すマグニチュードは" + Json["Body"]["Earthquake"]["Magnitude"].Value<string>() + "と推定されます。";
+                    }
+                    CommentLabel.Text = Json["Body"]["Comments"]["Observation"].Value<string>();
+                    Last_eventid = Json["Head"]["EventID"].Value<string>();
+                    Last_dateTime = Json["Control"]["DateTime"].Value<string>();
+                    TitleLabel.Text = Json["Head"]["Title"].Value<string>();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
